34bf648f72155281b17791d2894483f9af726f89
shushruthreddy17 <168461031+shushruthreddy17@users.noreply.github.com>
2026-05-12 20:01:28 -0500

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        write = 1
+
+        for read in range(1, len(nums)):
+
+            # found new unique number
+            if nums[read] != nums[read - 1]:
+
+                nums[write] = nums[read]
+                write += 1
+
+        return write
+
+        
